dff2be7b73a577df60f4d0726ce0686c069b27b1
Frederik Gailly <Frederik.Gailly@ugent.be>
2024-10-16 08:46:05 +0200

    lecture 3

---
+++ Lecture3/ComputeFactorialTailRecursion.py
@@ -0,0 +1,16 @@
+def main():
+    n = int(input("Enter a nonnegative integer: "))
+    print("Factorial of", n, "is", factorial(n))
+
+# Return the factorial for a specified number 
+def factorial(n):
+    return factorialHelper(n, 1) # Call auxiliary function
+  
+# Auxiliary tail-recursive function for factorial 
+def factorialHelper(n, result):
+    if n == 0:
+        return result
+    else:
+        return factorialHelper(n - 1, n * result) # Recursive call
+
+main() # Call the main function
